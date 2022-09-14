590ab4c841d0a7bc42df173b77928ce70adf253e
Ferytom <garuda.bangkit@hotmail.com>
2022-09-14 15:11:06 +0700

    Gambar 3 titik

--- main.js
+++ main.js
@@ -2,12 +2,30 @@ function main() {
   var kanvas = document.getElementById('kanvas');
   var gl = kanvas.getContext('webgl');
 
+  var vertices = [
+    0.5,
+    0.5, // A: kanan atas
+    0.0,
+    0.0, // B: bawah tengah
+    -0.5,
+    0.5, // C: kiri atas
+  ];
+
+  var buffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+  gl.bufferData(
+    gl.ARRAY_BUFFER,
+    new Float32Array(vertices),
+    gl.STATIC_DRAW
+  );
+
   // Vertex shader
   var vertexShaderCode = `
+    attribute vec2 aPosition;
     void main() {
-      float x = 0.0;
-      float y = 0.0;
-      gl_PointSize = 100.0;
+      float x = aPosition.x;
+      float y = aPosition.y;
+      gl_PointSize = 80.0;
       gl_Position = vec4(x, y, 0.0, 1.0);
     }
   `;
@@ -19,10 +37,10 @@ function main() {
   var fragmentShaderCode = `
     precision mediump float;
     void main() {
-      // float r = 0.0;
-      // float g = 0.0;
-      // float b = 1.0;
-      gl_FragColor = vec4(0.0, 0.0, 1.0, 1.0);
+      float r = 0.0;
+      float g = 0.0;
+      float b = 1.0;
+      gl_FragColor = vec4(r, g, b, 1.0);
     }
   `;
   var fragmentShaderObject = gl.createShader(gl.FRAGMENT_SHADER);
@@ -35,9 +53,16 @@ function main() {
   gl.linkProgram(shaderProgram);
   gl.useProgram(shaderProgram);
 
+  // Kita mengajari GPU bagaimana caranya mengoleksi
+  // nilai posisi dari ARRAY_BUFFER
+  // untuk setiap verteks yang sedang diproses
+  var aPosition = gl.getAttribLocation(shaderProgram, 'aPosition');
+  gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(aPosition);
+
   gl.clearColor(1.0, 0.65, 0.0, 1.0);
   //            Merah, Hijau, Biru, Transparansi
   gl.clear(gl.COLOR_BUFFER_BIT);
 
-  gl.drawArrays(gl.POINTS, 0, 1);
+  gl.drawArrays(gl.POINTS, 0, 3);
 }
