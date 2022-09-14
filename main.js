eb3d3df7515472091f8cb3449d3498c9a40751d3
Ferytom <garuda.bangkit@hotmail.com>
2022-09-14 14:50:01 +0700

    membuat kotak dalam kotak

--- main.js
+++ main.js
@@ -5,21 +5,29 @@ function main() {
   // Vertex shader
   var vertexShaderCode = `
     void main() {
-
-    }`;
+      float x = 0.0;
+      float y = 0.0;
+      gl_PointSize = 100.0;
+      gl_Position = vec4(x, y, 0.0, 1.0);
+    }
+  `;
   var vertexShaderObject = gl.createShader(gl.VERTEX_SHADER);
   gl.shaderSource(vertexShaderObject, vertexShaderCode);
   gl.compileShader(vertexShaderObject);
 
   // Fragment shader
   var fragmentShaderCode = `
+    precision mediump float;
     void main() {
-
+      // float r = 0.0;
+      // float g = 0.0;
+      // float b = 1.0;
+      gl_FragColor = vec4(0.0, 0.0, 1.0, 1.0);
     }
-    `;
+  `;
   var fragmentShaderObject = gl.createShader(gl.FRAGMENT_SHADER);
-  gl.shaderSource(vertexShaderObject, vertexShaderCode);
-  gl.compileShader(vertexShaderObject);
+  gl.shaderSource(fragmentShaderObject, fragmentShaderCode);
+  gl.compileShader(fragmentShaderObject);
 
   var shaderProgram = gl.createProgram(); // wadah dari executable (.exe)
   gl.attachShader(shaderProgram, vertexShaderObject);
@@ -27,7 +35,9 @@ function main() {
   gl.linkProgram(shaderProgram);
   gl.useProgram(shaderProgram);
 
-  gl.clearColor(1.0, 0.5, 0.0, 1.0);
+  gl.clearColor(1.0, 0.65, 0.0, 1.0);
   //            Merah, Hijau, Biru, Transparansi
   gl.clear(gl.COLOR_BUFFER_BIT);
+
+  gl.drawArrays(gl.POINTS, 0, 1);
 }
